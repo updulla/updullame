65aedc3b6830483a41f529347bcf66c48e520d5a
updulla <abdullah.boussount@gmail.com>
2022-06-19 21:02:34 +0100

    here

--- src/views/Preview.jsx
+++ src/views/Preview.jsx
@@ -1,32 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const previewDemo = [
-  {
-    img: "dark",
-    title: "Dark Demo (Professional Portfolio)",
-    routerPath: "/home-dark",
-    delayAnimation: "50",
-  },
-  {
-    img: "light",
-    title: "Light Demo (Classic Portfolio)",
-    routerPath: "/home-light",
-    delayAnimation: "",
-  },
-];
+
 
 const Preview = () => {
   return (
     <div>
-      <section className="banner text-center">
-        <div className="content">
-          <h1>Tunis</h1>
-          <h2>Personal Portfolio React Template</h2>
-        </div>
-      </section>
-      {/* End .banner */}
-
       <section className="demo dark">
         <div className="container">
           <div className="row">
@@ -68,48 +47,6 @@ const Preview = () => {
         </div>
       </section>
       {/* End .Demo */}
-
-      <footer>
-        <div className="go_purchase">
-          <h6 data-aos="fade-up" data-aos-duration="1200">
-            You are at the right step now
-          </h6>
-          <h3 data-aos="fade-up" data-aos-duration="1200" data-aos-delay="100">
-            Purchase Tunis & Build Your Super Fast{" "}
-            <span className="theme-color">React</span> &amp;{" "}
-            <span className="theme-color">Bootstrap 5 </span> Based Portfolio
-            Template.
-          </h3>
-          <div data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200">
-            <a
-              className="button"
-              href="https://themeforest.net/user/ib-themes/portfolio"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span className="button-text"> Purchase Now</span>
-              <span className="button-icon fa fa-arrow-right"></span>
-            </a>
-          </div>
-        </div>
-        {/* End go_purchase */}
-
-        <div className="text-center footer_copyright">
-          <h6>Tunis - Personal Portfolio React Template</h6>
-          <h5>
-            &copy; {new Date().getFullYear()} Designed with{" "}
-            <span className="heart">&#10084;</span> by{" "}
-            <a
-              href="https://themeforest.net/user/ib-themes"
-              target="_blank"
-              rel="noreferrer"
-            >
-              ib-themes.
-            </a>{" "}
-          </h5>
-        </div>
-      </footer>
-      {/* End footer */}
     </div>
   );
 };
